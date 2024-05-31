99786a80f99b1b4be78a08a2bc60281c50d0e4da
Ahmed Maged <ahmedmaged20188@gmail.com>
2024-05-31 09:51:35 +0300

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto p-3">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat facilis
+        officia sit velit nobis aliquam tempora rerum maxime quod fuga modi
+        laboriosam molestiae doloribus distinctio, commodi qui neque minus
+        facere.
+      </p>
+    </div>
+  );
+}
